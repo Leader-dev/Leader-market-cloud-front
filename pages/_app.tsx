00be2c38e844c8ae5d973752c06834c43434f8cf
Astatine-213-Tian <astatine213.tian@gmail.com>
2022-05-18 23:35:27 -0400

    fix: new icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,7 +20,7 @@ function App({ Component, pageProps }: AppProps) {
         <ChakraProvider theme={theme}>
           <Fonts />
           <Head>
-            <link rel="icon" href="/favicon.ico" />
+            <link rel="icon" href="/favicon.png" />
           </Head>
           <Component {...pageProps} />
         </ChakraProvider>
