130f168a18f270e753eb83280a3fa0a7ec37c757
Astatine-213-Tian <astatine213.tian@gmail.com>
2022-06-11 18:35:49 -0400

    style: auto style code

--- services/user/getAuthcode.ts
+++ services/user/getAuthcode.ts
@@ -1,7 +1,5 @@
 import { request } from "utils/request";
 
 export const getAuthCode = async (params: { phone: string }) => {
-  return await request
-    .post("/user/authcode", params)
-    .then(({ data }) => data);
+  return await request.post("/user/authcode", params).then(({ data }) => data);
 };
