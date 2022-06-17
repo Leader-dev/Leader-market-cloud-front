c514a627235cae9cd3cb38ffd2ea5d60a0436b54
Astatine-213-Tian <astatine213.tian@gmail.com>
2022-06-16 21:08:44 -0400

    fix: wrong url

--- services/org/manage/getOrgManageList.ts
+++ services/org/manage/getOrgManageList.ts
@@ -3,6 +3,6 @@ import { createService } from "services/index";
 import { Org } from "types/user";
 
 export default createService<Org[]>({
-  url: () => "/mc/org/list",
+  url: () => "/mc/org/manage/list",
   get: (url) => request.post(url).then(({ data }) => data.list),
 });
